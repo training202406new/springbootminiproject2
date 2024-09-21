7d22f23eab7947d4fe4891a0033addf558244bfa
saikiran1045 <dodlakiran789@gmail.com>
2024-09-21 10:48:47 +0530

    Initial commit

---
+++ src/test/java/com/example/springbootminiproject2/Springbootminiproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootminiproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootminiproject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
